7875c90808a83edba44cd69ce33d25b682dccbc8
icbc <icbc@icbcdeMacBook-Air.local>
2017-08-01 14:21:57 +0800

    init

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'demo' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for demo
+
+  target 'demoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'demoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by icbc on 2017/8/1.
+//  Copyright © 2017年 carolxfyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
